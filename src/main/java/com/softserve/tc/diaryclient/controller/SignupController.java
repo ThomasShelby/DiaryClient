2f82299fb5732e9fbecd59ad841e4553384e43c7
Bug5Bunny <mural@ex.ua>
2015-10-12 16:09:39 +0300

    registration ready

---
+++ src/main/java/com/softserve/tc/diaryclient/controller/SignupController.java
@@ -0,0 +1,50 @@
+package com.softserve.tc.diaryclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.softserve.tc.diaryclient.entity.SignupForm;
+import com.softserve.tc.diary.entity.User;
+import com.softserve.tc.diary.webservice.DiaryService;
+import com.softserve.tc.diaryclient.validator.SignupValidator;
+import com.softserve.tc.diaryclient.webservice.diary.DiaryServiceConnection;
+
+@Controller
+@RequestMapping("/signup")
+public class SignupController {
+        @Autowired
+        private SignupValidator signupValidator;
+
+        @RequestMapping(method = RequestMethod.GET)
+        public String signup(ModelMap model) {
+                SignupForm signupForm = new SignupForm();
+                model.put("signupForm", signupForm);
+                return "signup";
+        }
+
+        @RequestMapping(method = RequestMethod.POST)
+        public ModelAndView processSignup(SignupForm signupForm, BindingResult result) {
+                signupValidator.validate(signupForm, result);
+                ModelAndView model = new ModelAndView();
+                model.setViewName("login");
+                if (result.hasErrors()) {
+                    model.addObject("error", "Registration failed!");
+                    return model;
+                }
+                model.addObject("msg", "You've been registered successfully.");
+                model.addObject("title", "The Diary Log In");
+                User user = new User();
+                user.setNickName(signupForm.getUsername());
+                user.setPassword(signupForm.getPassword());
+                user.seteMail(signupForm.getEmail());
+                user.setRole("USER");
+                DiaryService port = DiaryServiceConnection.getDairyServicePort();
+                port.createUser(user);
+                return model;
+        }
+}

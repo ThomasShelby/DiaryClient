85297427fdfdc966cff214973f9d55677b5d835b
Maryana Bytsiv <Maryana Bytsiv>
2015-10-27 19:07:42 +0200

    autocomplete fix

--- src/main/java/com/softserve/tc/diaryclient/controller/AutocompleteController.java
+++ src/main/java/com/softserve/tc/diaryclient/controller/AutocompleteController.java
@@ -11,45 +11,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
+import com.softserve.tc.diary.entity.Tag;
 import com.softserve.tc.diaryclient.webservice.diary.DiaryServiceCashLoader;
 
 @Controller
 public class AutocompleteController {
 
-	private static List<String> data = new ArrayList<String>();
-	
-	@Autowired
-	public DiaryServiceCashLoader diaryServiceCashLoader;
-	
-	@RequestMapping(value = "/getTags", method = RequestMethod.GET)
-	public @ResponseBody
-	String getTags(@RequestParam String tag) {
-		
-		System.out.println(tag);
-		if (tag.startsWith("#")) {
-			//get data directly from service
-			//DiaryService port = DiaryServiceConnection.getDairyServicePort();
-			//data = port.getListTagsBPrefix(tag);
+  private static List<Tag> data = new ArrayList<Tag>();
 
-			//get data (hashes) from client cash
-			data = diaryServiceCashLoader.getListOfHashTagsFromCash();
+  @Autowired
+  public DiaryServiceCashLoader diaryServiceCashLoader;
 
-			return new Gson().toJson(simulateSearchResult(tag));	
-		}
-		return null;
-	}
+  @RequestMapping(value = "/getTags", method = RequestMethod.GET)
+  public @ResponseBody
+  String getTags(@RequestParam String tag) {
 
-	private List<String> simulateSearchResult(String tagName) {
+    System.out.println(tag);
+    if (tag.startsWith("#")) {
+      //get data directly from service
+      //DiaryService port = DiaryServiceConnection.getDairyServicePort();
+      //data = port.getListTagsBPrefix(tag);
 
-		List<String> result = new ArrayList<String>();
+      //get data (hashes) from client cash
+      data = diaryServiceCashLoader.getListOfHashTagsFromCash();
 
-		// iterate a list and filter by tagName
-		for (String tag : data) {
-			if (tag.contains(tagName)) {
-				result.add(tag);
-			}
-		}
-		
-		return result;
-	}
+      return new Gson().toJson(simulateSearchResult(tag));	
+    }
+    return null;
+  }
+
+  private List<Tag> simulateSearchResult(String tagName)  {
+
+    List<Tag> result = new ArrayList<Tag>();
+
+    // iterate a list and filter by tagName
+    for (Tag tag : data) {
+      if (tag.getTagMessage().contains(tagName)) {
+        result.add(tag);
+      }
+    }
+
+    return result;
+  }
 }
